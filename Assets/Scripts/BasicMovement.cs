696893e7f1c2cd7b93ca5208f59672e421b994ad
Erik Salazar <3salaz.dev@gmail.com>
2020-06-29 22:26:14 -0700

    added horizonal movement & animation

--- Assets/Scripts/BasicMovement.cs
+++ Assets/Scripts/BasicMovement.cs
@@ -4,19 +4,19 @@ using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    public Animator animator;
+
+
+
         //  Input.GetAxis("Horizontal") - give back a number between -1 to 1 depending on which arrow keys we press ~>
         // 
         // change tranform.position on X axis , this position is a 3d vector which uses 3 variables to select a point in space  
-    }
 
     // Update is called once per frame
     void Update()
     {
+        animator.SetFloat("Horizontal", Input.GetAxis("Horizontal"));
         Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + horizontal * Time.deltaTime;
-          
     }
 }

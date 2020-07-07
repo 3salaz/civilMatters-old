6374c280bcb215bfc1ab16490e9eb02171cd858e
Erik Salazar <3salaz.dev@gmail.com>
2020-07-07 11:53:57 -0700

    added new assets for player

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // defaults to 50 times a second
+    void fixedUpdate()
+    {
+        
+    }
+}
